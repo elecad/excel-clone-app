4d6431dd9e5132ad1b00b4da1c14db795b52721b
Anton <relas1234@gmail.com>
2020-08-11 00:02:52 +0300

    Финальная вёрстка проекта

--- webpack.config.js
+++ webpack.config.js
@@ -81,7 +81,7 @@ module.exports = {
                         }
                     },
                     'css-loader',
-                    'sass-loader',
+                    'sass-loader'
                 ],
             },
             {
