f8a8188dbd3ec2617b1b2c09068d2b0462f432b5
Anton <relas1234@gmail.com>
2020-08-15 23:27:10 +0300

    Финальная версия базы фреймворка

---
+++ src/components/table/Table.js
@@ -0,0 +1,11 @@
+import {createTable} from "@/components/table/table.template";
+import {ExcelComponent} from "@core/ExcelComponent";
+
+export class Table extends ExcelComponent {
+
+    static className = "excel__table"
+
+    toHTML() {
+        return createTable(30)
+    }
+}
